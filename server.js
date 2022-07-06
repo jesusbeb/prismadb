a8697fd654af2a4d382431e60a365466df7df679
jesusbeb <jbeltranb@outlook.com>
2022-07-05 20:17:22 -0500

    Creando un server básico para agregar el cliente de Prisma

---
+++ server.js
@@ -0,0 +1,17 @@
+// Crear un server básico agregando el cliente de Prisma
+
+const express = require ('express');
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+// Require para usar Prisma
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+app.get('/', (req,res) => {
+    res.json({message: 'alive'});
+});
+app.listen(port,() => {
+    console.log(`Listening to requests on port ${port}`);
+});
